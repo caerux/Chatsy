acf4c0ece4b785cbb42bd34806c946cbf35f212e
caerux <ak.ankitkumar0202@gmail.com>
2021-08-08 20:15:09 +0530

    My First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="52ea2caf-a7e4-491e-98e9-bc56a511879b"
+            userName="traitor"
+            userSecret="user1"
+        />
+    );
+}
+
+export default App;
