e4080030b04e5e449e35aa896d4d8b635fd6f155
caerux <ak.ankitkumar0202@gmail.com>
2021-08-09 15:37:44 +0530

    third commit

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
 
 const ChatFeed = (props) => {
-    console.log(props);
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+
+    console.log(chat, userName, messages);
 
     return (
         <div>
