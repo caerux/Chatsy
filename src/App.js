2f01f3d9448b1227602a3527b9b7e2e927a22b9c
caerux <ak.ankitkumar0202@gmail.com>
2021-08-09 21:15:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from  './components/LoginForm';
 import ChatFeed from './components/ChatFeed'; 
 
 import './App.css';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return(
         <ChatEngine
             height="100vh"
             projectID="52ea2caf-a7e4-491e-98e9-bc56a511879b"
-            userName="traitor"
-            userSecret="user1"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
